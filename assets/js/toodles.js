86d3332976ac030dbedbdbc2055d71866bb000fe
Cam Swords <cam.swords@gmail.com>
2015-09-06 11:36:30 +1000

    added Zolmeister's game to the page.

--- assets/js/toodles.js
+++ assets/js/toodles.js
@@ -7,4 +7,10 @@ window.$ = $;
 
 $(document).ready(function() {
     modules.execute($('.dynamic-module'));
+    
+    $('#mute').click(function() {
+        if (window.toggleSound) { 
+            window.toggleSound(); 
+        }
+    });
 });
