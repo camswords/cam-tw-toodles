3836d4849c614ea0f67c6e58468efb1ec6411baa
Cam Swords <cam.swords@gmail.com>
2015-09-06 11:10:35 +1000

    updated the url for prod

--- src/environments/production.js
+++ src/environments/production.js
@@ -2,6 +2,6 @@
 module.exports = {
     environment: function() { return "production"; },
     port: function() { return Number(process.env.PORT); },
-    url: function() { return 'http://www.foobar.com/'; },
+    url: function() { return 'https://toodles-tw.herokuapp.com/'; },
     googleAnalyticsApiKey: function() { return process.env.GOOGLE_ANALTICS_TRACKING_ID; }
 };
